7174b02eb430d76b3c01900078f04b5004bd1b7a
leonardopad <77343517+leonardopad@users.noreply.github.com>
2021-10-13 10:55:40 -0300

    Add files via upload

--- q6.cpp
+++ q6.cpp
@@ -7,10 +7,10 @@ int dado(int parametro){
     return (1 + rand()%6);
 }
 int main(){
-	int d, num1, num2, num3, num4, num5, num6;
+	int d[0], num1, num2, num3, num4, num5, num6;
 
 	for(int i=0 ; i <= 100 ; i++){
-       std:: cout << dado(d);
+       std:: cout << dado(d[])= dado[i];
 	}	
 //		std:: cout << num1 << "\n" << num2 << "\n" << num3 << "\n" << num4 << "\n" << num5 << "\n" << num6 << "\n";
 	return 0;
